33a0bece0ed20c8170c9b156a262991e0b456f38
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2020-12-15 12:42:21 +0100

    Hola

--- Entornos/src/Prueba1.java
+++ Entornos/src/Prueba1.java
@@ -1,4 +1,13 @@
-
+import java.util.Scanner;
 public class Prueba1 {
+	public static void main(String[] args) {
+	    Scanner teclado;
+	    teclado = new Scanner(System.in);
+	    float numero1;
 
+	    System.out.println("Dime las millas por favor");
+	    numero1=teclado.nextFloat();
+	    System.out.println("El resultado en metros es el siguiente:");
+	    System.out.println(numero1 * 1609);
+	 }
 }
